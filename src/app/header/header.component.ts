9a4adc2b540f09e632a0e49de2feacd9063b5822
ignacio pugliese <ignacio.pugliese@10pines.com>
2020-10-02 13:51:36 -0300

    boton mejorado

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -42,4 +42,7 @@ export class HeaderComponent implements OnInit {
   dropdownToggle(){
     this.isDropdownOpen = !this.isDropdownOpen
   }
+  hideDropdown(){
+    this.isDropdownOpen = false;
+  }
 }
