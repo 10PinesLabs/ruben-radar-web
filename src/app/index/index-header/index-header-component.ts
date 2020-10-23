5c813c0c41f9d642159ba5863423a849d118f20f
belu <belen.amat29@gmail.com>
2020-10-23 13:11:35 -0300

    agrego dependencia de ngx-bootstrap para modales. Agrego modal básico

--- src/app/index/index-header/index-header-component.ts
+++ src/app/index/index-header/index-header-component.ts
@@ -1,14 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, TemplateRef} from '@angular/core';
 import {Router} from "@angular/router";
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import {ComponentLoaderFactory} from 'ngx-bootstrap/component-loader';
+import {PositioningService} from 'ngx-bootstrap/positioning';
 
 @Component({
   selector: 'app-index-header-component',
   templateUrl: './index-header-component.html',
-  styleUrls: ['./index-header-component.scss']
+  styleUrls: ['./index-header-component.scss'],
+  providers: [BsModalService, ComponentLoaderFactory, PositioningService]
 })
 export class IndexHeaderComponent {
+  modalRef: BsModalRef;
+  constructor(private router: Router, private modalService: BsModalService) {}
 
-  constructor(private router: Router) {}
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
 
   navigateToCreateRadarTemplate = () => {
     this.router.navigate(['radarTemplate/create']);
