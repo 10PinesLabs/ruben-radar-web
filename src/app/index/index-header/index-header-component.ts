8c19492beebf8ce71540968e8cb635c09d722338
belu <belen.amat29@gmail.com>
2020-10-27 11:50:03 -0300

    se crea un radar template desde el frontend

--- src/app/index/index-header/index-header-component.ts
+++ src/app/index/index-header/index-header-component.ts
@@ -1,9 +1,11 @@
-import {Component, Input, OnInit, TemplateRef} from '@angular/core';
+import {Component, Inject, Input, OnInit, TemplateRef} from '@angular/core';
 import {Router} from "@angular/router";
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import {ComponentLoaderFactory} from 'ngx-bootstrap/component-loader';
 import {PositioningService} from 'ngx-bootstrap/positioning';
 import {Axis} from '../../../model/axis';
+import {RadarTemplate} from '../../../model/radarTemplate';
+import {RadarTemplateService} from '../../../services/radarTemplate.service';
 
 @Component({
   selector: 'app-index-header-component',
@@ -13,14 +15,15 @@ import {Axis} from '../../../model/axis';
 })
 export class IndexHeaderComponent implements OnInit {
   @Input() radarTemplateContainers;
-  selectedRadarTemplateContainer = null;
+  selectedRadarTemplateContainerId = null;
   radarTemplateName = '';
   radarTemplateDescription = '';
   newAxisName = '';
   newAxisDescription = '';
   radarTemplateAxes: Axis[];
   modalRef: BsModalRef;
-  constructor(private router: Router, private modalService: BsModalService) {}
+  constructor(private router: Router, private modalService: BsModalService,
+              @Inject('RadarTemplateService') private radarTemplateService: RadarTemplateService,) {}
 
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
@@ -32,7 +35,6 @@ export class IndexHeaderComponent implements OnInit {
     if (this.validNewAxis()) {
       const newAxis = new Axis(null, this.newAxisName, this.newAxisDescription, null);
       this.radarTemplateAxes.push(newAxis);
-      console.log(this.radarTemplateAxes);
       this.newAxisName = '';
       this.newAxisDescription = '';
     }
@@ -42,8 +44,21 @@ export class IndexHeaderComponent implements OnInit {
     this.radarTemplateAxes = this.radarTemplateAxes.filter(a => a.name !== axis.name);
   }
 
+  axisHasDescription(axis) {
+    return !!axis.description;
+  }
+
   validNewAxis() {
     return !!this.newAxisName;
+  }
+
+  onChange(value) {
+    this.selectedRadarTemplateContainerId = value;
+  }
+
+  createRadarTemplate() {
+    const newRadarTemplate = new RadarTemplate(null, this.selectedRadarTemplateContainerId, this.radarTemplateName, this.radarTemplateDescription, this.radarTemplateAxes, null, []);
+    this.radarTemplateService.create(newRadarTemplate).subscribe(() => this.router.navigate(['/radarTemplates']));
   }
 
   navigateToCreateRadarTemplate = () => {
