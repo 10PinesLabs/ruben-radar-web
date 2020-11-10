0821d324384fb21275094231cb29cfa184fa60be
ignacio pugliese <ignacio.pugliese@10pines.com>
2020-11-10 12:17:02 -0300

    los pins son dos listas

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit, Inject} from '@angular/core';
 import {Router} from "@angular/router";
+import {Observable, of } from 'rxjs';
+import { forkJoin } from 'rxjs';
 import {RadarTemplateContainer} from "../../model/radarTemplateContainer";
 import {RadarTemplateContainerService} from "../../services/radarTemplateContainer.service";
 
@@ -11,6 +13,8 @@ import {RadarTemplateContainerService} from "../../services/radarTemplateContain
 export class IndexComponent implements OnInit {
 
   radarTemplateContainers: RadarTemplateContainer[];
+  pinnedTemplateContainers : RadarTemplateContainer[];
+  unpinnedTemplateConteinars : RadarTemplateContainer[];
 
   constructor(@Inject('RadarTemplateContainerService') private radarTemplateContainerService: RadarTemplateContainerService,
               private router: Router) {
@@ -24,15 +28,51 @@ export class IndexComponent implements OnInit {
           radarTemplateContainer.description, radarTemplateContainer.active, radarTemplateContainer.radar_templates,
           radarTemplateContainer.active_voting_code));
       })
+
+      let pinContainersStatusRequest : Observable<boolean>[] = this.radarTemplateContainers.map(container => this.radarTemplateContainerService.isPinned(container.id))
+      forkJoin(pinContainersStatusRequest).subscribe((pinContainersStatus) => {
+        const pinnedContainers = this.radarTemplateContainers.filter((c, index) => pinContainersStatus[index] )
+        const unpinnedContainers = this.radarTemplateContainers.filter( contianer => !pinnedContainers.includes(contianer))
+        this.pinnedTemplateContainers = pinnedContainers;
+        this.unpinnedTemplateConteinars = unpinnedContainers;
+      })
     });
   }
 
-  async pinnedRadarTemplateContainers(){
-    return new Promise((res)=> res(this.radarTemplateContainers.filter( container => container.isPinned())))
+  pinToggle(container){
+    this.isRadarTemplateConainerPinned(container) ? this.unpinContainer(container) : this.pinContainer(container)
   }
 
-  unpinnedRadarTemplateContainers(){
-    return this.radarTemplateContainers.filter( container => !container.isPinned())
+  pinContainer(container : RadarTemplateContainer){
+    this.radarTemplateContainerService.pin(container.id).subscribe(()=>{
+      const indexOfPinnedContaier = this.unpinnedTemplateConteinars.indexOf(container)
+      this.unpinnedTemplateConteinars.splice(indexOfPinnedContaier);
+      this.pinnedTemplateContainers.push(container)
+    })
+  }
+
+  unpinContainer(container : RadarTemplateContainer){
+    this.radarTemplateContainerService.unpin(container.id).subscribe(()=>{
+      const indexOfUnpinnedContaier = this.pinnedTemplateContainers.indexOf(container)
+      this.pinnedTemplateContainers.splice(indexOfUnpinnedContaier);
+      this.unpinnedTemplateConteinars.push(container)
+    })
+  }
+
+  isRadarTemplateConainerPinned(c){
+    return this.pinnedTemplateContainers.includes(c)
+  }
+
+  orderContainers(){
+    this.radarTemplateContainers = this.radarTemplateContainers.sort((a : RadarTemplateContainer, b : RadarTemplateContainer)=>{
+        if(this.pinnedTemplateContainers.includes(a) && !this.pinnedTemplateContainers.includes(b)){
+          return 1
+        }else if(this.pinnedTemplateContainers.includes(a) && this.pinnedTemplateContainers.includes(b)){
+          return -1
+        }else{
+          return 0
+        }
+    })
   }
 
   navigateToCreateRadarTemplate = () => {
