730ffd0844af42aadaa1ca0b66b7d4720f5670cb
Carolina <carolinadestuet@gmail.com>
2020-11-11 17:21:52 -0300

    especifico tipo de método close en voting service

--- src/services/voting.service.ts
+++ src/services/voting.service.ts
@@ -1,5 +1,8 @@
+import {Observable} from 'rxjs';
+import {Voting} from '../model/voting';
+
 export interface VotingService {
   create(radarTemplateContainerId: string, name: string, ends_at: string): any;
   get(code: string): any;
-  close(radarTemplateContainerId: string): any;
+  close(radarTemplateContainerId: string): Observable<Voting>;
 }
