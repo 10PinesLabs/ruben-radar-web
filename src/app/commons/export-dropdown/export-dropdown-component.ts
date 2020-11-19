2864328f21f3aa3c453ce8942bff5b4e9b07384f
belu <belen.amat29@gmail.com>
2020-11-19 11:14:37 -0300

    Set up inicial de PDF

--- src/app/commons/export-dropdown/export-dropdown-component.ts
+++ src/app/commons/export-dropdown/export-dropdown-component.ts
@@ -1,5 +1,9 @@
 import {Component, Input} from '@angular/core';
 import {ngxCsv} from 'ngx-csv';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+import {RadarTemplateContainer} from '../../../model/radarTemplateContainer';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-export-dropdown',
@@ -10,11 +14,32 @@ export class ExportDropdownComponent {
   @Input() data = [];
   @Input() headers = [];
   @Input() filename = 'rawData';
+  @Input() radarTemplateContainer: RadarTemplateContainer;
 
   resultsCSV = () =>  {
     const options = {
       headers: this.headers,
     };
     return new ngxCsv(JSON.stringify(this.data), this.filename + '.csv', options);
+  }
+
+  generatePDF = () => {
+    const docDefinition = {
+      header: {
+        text: this.radarTemplateContainer.name,
+        style: 'sectionHeader'
+      },
+      styles: {
+        sectionHeader: {
+          margin: [15,15,15,15],
+          alignment: 'center',
+          bold: true,
+          decoration: 'underline',
+          fontSize: 24,
+        }
+      }
+    };
+
+    pdfMake.createPdf(docDefinition).open();
   }
 }
